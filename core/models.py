e111fd15950841b9e08a1149da3ed867e35d25ec
Okano Takumi <oknokn.of.12@gmail.com>
2020-10-21 17:49:58 +0900

    make serializer

--- core/models.py
+++ core/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
-
+#プロフィールテーブル
 class Profile(models.Model):
     nickName = models.CharField(max_length=20)
     userPro = models.OneToOneField(
